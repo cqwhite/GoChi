e49a6be875e42b2f58b3cc7be158602df560ee92
Connor White <cqwhite19@gmail.com>
2021-11-14 20:09:00 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Howdy"))
+	})
+	fmt.Println("App is running at http://localhost:3000")
+	http.ListenAndServe(":3000", r)
+
+}
